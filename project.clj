9899575d67365e0872033d06a0c555bd3952a4d8
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2018-04-24 14:45:09 +0200

    Add project.clj #1

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject hugsql-adapter-case "0.0.0"
+  :description "A HugSQL adapter that converts name of SQL columns"
+  :url "https://github.com/druids/hugsql-adapter-case"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+
+  :dependencies [[camel-snake-kebab "0.4.0"]]
+
+
+  :profiles {:dev {:plugins [[lein-cloverage "1.0.10"]
+                             [lein-kibit "0.1.6"]
+                             [jonase/eastwood "0.2.5"]]
+                   :eastwood {:exclude-linters [:constant-test]} ;; due false positive alert in if-let* macro
+
+                   :dependencies [[org.clojure/clojure "1.9.0"]]}})
